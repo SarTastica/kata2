c74a50b4507ce811823038890a29a068c33c050a
SarTastica <sartastica@gmail.com>
2025-11-16 20:24:53 +0000

    Title done

---
+++ src/Title.java
@@ -0,0 +1,25 @@
+public class Title {
+    private final String id;
+    private final String name;
+
+    public Title(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Title{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
