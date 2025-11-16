640668898e14d8a9de495da2869a437ec6d4686a
SarTastica <sartastica@gmail.com>
2025-11-16 20:28:08 +0000

    TsvTitleDeserializer done

---
+++ src/TsvTitleDeserializer.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TsvTitleDeserializer implements TitleDeserializer{
+    @Override
+    public List<Title> deserialize(List<String> lines) {
+        List<Title> titles = new ArrayList<>();
+
+        for(String line : lines){
+            String[] fields ) line.split("\t");
+            if(fields.length < 3) continue;
+            titles.add(new Title(fields[0], fields[2]));
+        }
+        return titles
+    }
+}
